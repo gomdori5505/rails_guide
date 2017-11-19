81882cd2554816727302353134411b36289668c5
gomdori5505 <gomdori5505@nate.com>
2017-11-19 12:43:19 +0900

    create_bulletin_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118115058) do
+ActiveRecord::Schema.define(version: 20171119032429) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
